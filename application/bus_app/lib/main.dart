ab8be8a040bb06ac28b62a5de0d9a4b85e099cb7
anushkar <anushka.ranasinghe@grabone.co.nz>
2023-09-21 16:45:37 +1200

    Map Submit with Login template

---
+++ application/bus_app/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:bus_app/main_login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MainPage(),
+    );
+  }
+}
---
+++ application/bus_app/lib/main_map_page.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:bus_app/locations.dart' as locations;
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPage();
+}
+
+class _MapPage extends State<MapPage> {
+  final Map<String, Marker> _markers = {};
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor markerCurrentIcon = BitmapDescriptor.defaultMarker;
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    //final response = await rootBundle.loadString('/json/stops.json');
+    //   final data = await json.decode(response);
+    final googleOffices = await rootBundle.loadString('/json/stops.json');
+    final data = await json.decode(googleOffices);
+    final inspect = data['data'];
+
+    setState(() {
+      _markers.clear();
+
+      for (final stops in inspect) {
+        final marker = Marker(
+          markerId: MarkerId(stops['id']),
+          position: LatLng(
+              stops['attributes']['stop_lat'], stops['attributes']['stop_lon']),
+          infoWindow: InfoWindow(
+            title: stops['attributes']['stop_name'],
+          ),
+          icon: markerIcon,
+        );
+        _markers[stops['id']] = marker;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    addCustomIcon();
+    getCurrenttLocation();
+    super.initState();
+  }
+
+  getCurrenttLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    debugPrint('location lat: ${position.latitude}');
+    debugPrint('location lan: ${position.longitude}');
+    setState(() {
+      var currentLocation = [position.latitude, position.longitude];
+    });
+  }
+
+  addCustomIcon() async {
+    var iconNormal = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(40, 40)),
+        "assets/images/destination_map_marker.png");
+
+    setState(() {
+      markerIcon = iconNormal;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepPurpleAccent,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bus me Route'),
+          backgroundColor: Colors.deepPurpleAccent,
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(-36.914060, 174.882880),
+            zoom: 13,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
+  }
+}
