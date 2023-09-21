2fe7cbfcad99fa4e2f6056acc7b8028e2be71902
neutrongreen <joshua@green.geek.nz>
2023-09-21 14:50:40 +1200

    Create user management stucture

--- application/bus_me/lib/models/user_management.dart
+++ application/bus_me/lib/models/user_management.dart
@@ -1,6 +1,10 @@
 
 
+import 'dart:io';
+
 import 'package:bus_me/models/user_model.dart';
+
+import 'auth_model.dart';
 
 abstract class UserManagementModel
 {
@@ -15,4 +19,38 @@ abstract class UserManagementModel
 
   //update the user at ID to the new user object
   Future<int> updateUser(int id, User user);
+}
+
+class BusMEUserManagement extends UserManagementModel
+{
+  final AuthModel _authModel;
+
+  HttpClient userServer = HttpClient(context: SecurityContext.defaultContext);
+
+  BusMEUserManagement(this._authModel);
+
+  @override
+  Future<int> createUser(UserRegistration info, int userType) async {
+    // TODO: implement createUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> deleteUser(int id) async {
+    // TODO: implement deleteUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<User>> searchUser(String query) async{
+    // TODO: implement searchUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<int> updateUser(int id, User user)  async {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
+
 }
