fc892a2180575c26e07e4f56b73217b871e4c0d1
neutrongreen <joshua@green.geek.nz>
2023-09-21 14:11:23 +1200

    Add user registration and updating functions

--- application/bus_me/lib/models/user_model.dart
+++ application/bus_me/lib/models/user_model.dart
@@ -13,6 +13,12 @@ abstract class UserModel
   User? getUser();
   //update user
   Future<int> fetchUser();
+
+  //user function CANNOT CREATE ADMIN ACCOUNTS
+  Future<int> registerUser(UserRegistration user);
+
+  //user function for settings changes
+  Future<int> updateUser();
 }
 
 class BusMeUserModel extends UserModel
@@ -20,13 +26,11 @@ class BusMeUserModel extends UserModel
   User? _user;
   final AuthModel _authModel;
 
-  BusMeUserModel(this._authModel);
+  HttpClient userServer = HttpClient(context: SecurityContext.defaultContext);
 
-  @override
-  Future<int> fetchUser() async {
-    //build get request
-    HttpClient userServer = HttpClient(context: SecurityContext.defaultContext);
 
+  BusMeUserModel(this._authModel)
+  {
     userServer.badCertificateCallback =  (X509Certificate cert, String host, int port) {
       if (kDebugMode)
       {
@@ -35,6 +39,14 @@ class BusMeUserModel extends UserModel
       return false;
 
     };
+  }
+
+  @override
+  Future<int> fetchUser() async {
+    //build get request
+
+
+
 
     userServer.connectionTimeout = const Duration(seconds: 4);
 
@@ -81,6 +93,68 @@ class BusMeUserModel extends UserModel
   User? getUser() {
     return _user;
   }
+
+  @override
+  Future<int> registerUser(UserRegistration user) async {
+    Uri route = Uri.https(API_ROUTE,'/api/users/register');
+    HttpClientRequest postReq = await userServer.postUrl(route);
+
+    //write user info to network
+    postReq.write(jsonEncode(user));
+
+    HttpClientResponse result = await postReq.close();
+
+    if (result.statusCode != HttpStatus.created)
+    {
+      return 1;
+    }
+
+    return 0;
+  }
+
+  @override
+  Future<int> updateUser() async {
+
+    if (_user == null || !_authModel.isLoggedIn())
+    {
+      return 1;
+    }
+
+    Uri detailRoute = Uri.https(API_ROUTE,'/api/users/${_user!.id}}/details');
+    Uri settingsRotue = Uri.https(API_ROUTE,'/api/users/${_user!.id}}/settings');
+
+    HttpClientRequest detailPut = await userServer.putUrl(detailRoute);
+    HttpClientRequest settingsPut = await userServer.putUrl(settingsRotue);
+
+    detailPut.headers.add(HttpHeaders.authorizationHeader, _authModel.getToken()!);
+    settingsPut.headers.add(HttpHeaders.authorizationHeader, _authModel.getToken()!);
+
+    detailPut.write(jsonEncode(_user!.details));
+    settingsPut.write(jsonEncode(_user!.settings));
+
+    HttpClientResponse detailRes = await detailPut.close();
+    HttpClientResponse settingsRes = await settingsPut.close();
+
+    if (detailRes.statusCode != HttpStatus.ok || settingsRes.statusCode != HttpStatus.ok )
+    {
+      return 1;
+    }
+
+
+    return 0;
+  }
+
+
+}
+
+class UserRegistration {
+  String username;
+
+  String password;
+
+  UserDetails details;
+
+  UserRegistration(this.username, this.password, this.details);
 
 }
 
