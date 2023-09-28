ea34e6b6a0c27c3dc22436f46796861d728e1619
anushkar <anushka.ranasinghe@grabone.co.nz>
2023-09-28 21:53:28 +1300

    Updated map with current location, and its on map maker.

--- application/bus_me/lib/views/map_view.dart
+++ application/bus_me/lib/views/map_view.dart
@@ -1,70 +1,114 @@
-
 import 'package:flutter/material.dart';
-import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong2/latlong.dart';
-import 'settings_page.dart';
-import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'dart:async';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+//import 'package:bus_app/locations.dart' as locations;
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:location/location.dart';
+// import 'package:geocoding/geocoding.dart';
+// import 'package:geolocator/geolocator.dart';
 
-class MapView extends StatelessWidget {
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
   @override
-  Widget build(BuildContext context) {
-    return WillPopScope(
-        onWillPop: () async => false, // Disable back button
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('Map View'),
-            actions: [
-              IconButton(
-                icon: Icon(Icons.settings),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => SettingsPage(),
-                    ),
-                  );
-                },
-              ),
-            ],
-          ),
-          body: FlutterMap(
-            options: MapOptions(
-              center: LatLng(51.5, -0.09), //Will be overwritten by current location
-              zoom: 13.0,
-            ),
-            children: [
-              TileLayer(
-                urlTemplate:
-                    "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                subdomains: ['a', 'b', 'c'],
-              ),
-              CurrentLocationLayer(followOnLocationUpdate: FollowOnLocationUpdate.always,
-                turnOnHeadingUpdate: TurnOnHeadingUpdate.never,
-                style: LocationMarkerStyle(
-                  marker: const DefaultLocationMarker(
-                    child: Icon(
-                      Icons.navigation,
-                      color: Colors.white,
-                    ),
-                  ),
-                  markerSize: const Size(40, 40),
-                  markerDirection: MarkerDirection.heading,
-                ),),
-              MarkerLayer(
-                markers: [
-                  Marker(
-                    width: 80.0,
-                    height: 80.0,
-                    point: LatLng(51.5, -0.09),
-                    builder: (ctx) => Icon(Icons.pin_drop),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ));
-  }
+  State<MapPage> createState() => _MapPage();
 }
 
+class _MapPage extends State<MapPage> {
+  GoogleMapController? mapController;
+  final Map<String, Marker> _markers = {};
+  BitmapDescriptor markerIcon = BitmapDescriptor.defaultMarker;
+  BitmapDescriptor markerCurrentIcon = BitmapDescriptor.defaultMarker;
+  Location location = Location();
+  LatLng currentLocation = LatLng(-36.848461, 174.763336);
+  Marker currentLocationMarker = Marker(
+    markerId: MarkerId('current_location'),
+    position: LatLng(-36.848461, 174.763336),
+    infoWindow: InfoWindow(title: 'Current Location'),
+    icon: BitmapDescriptor.defaultMarker,
+  );
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    final googleOffices = await rootBundle.loadString('/json/stops.json');
+    final data = await json.decode(googleOffices);
+    final inspect = data['data'];
 
+    setState(() {
+      _markers.clear();
 
+      for (final stops in inspect) {
+        final marker = Marker(
+          markerId: MarkerId(stops['id']),
+          position: LatLng(
+              stops['attributes']['stop_lat'], stops['attributes']['stop_lon']),
+          infoWindow: InfoWindow(
+            title: stops['attributes']['stop_name'],
+          ),
+          icon: markerIcon,
+        );
+        _markers[stops['id']] = marker;
+      }
+    });
+  }
+
+  @override
+  void initState() {
+    addCustomIcon();
+    super.initState();
+    location.onLocationChanged.listen((LocationData newLocation) {
+      setState(() {
+        currentLocation = LatLng(newLocation.latitude!, newLocation.longitude!);
+
+        mapController?.animateCamera(
+          CameraUpdate.newLatLng(currentLocation),
+        );
+        currentLocationMarker = Marker(
+          markerId: MarkerId('current_location'),
+          position: currentLocation,
+          infoWindow: InfoWindow(title: 'Current Location'),
+          icon: BitmapDescriptor.defaultMarker,
+        );
+        _markers['current_location'] = currentLocationMarker;
+      });
+    });
+  }
+
+  addCustomIcon() async {
+    var iconNormal = await BitmapDescriptor.fromAssetImage(
+        ImageConfiguration(size: Size(40, 40)),
+        "assets/images/destination_map_marker.png");
+
+    setState(() {
+      markerIcon = iconNormal;
+      mapController?.animateCamera(
+        CameraUpdate.newLatLng(currentLocation),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.deepPurpleAccent,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Bus me Route'),
+          backgroundColor: Colors.deepPurpleAccent,
+          elevation: 2,
+        ),
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(-36.914060, 174.882880),
+            zoom: 13,
+          ),
+          markers: _markers.values.toSet(),
+        ),
+      ),
+    );
+  }
+}
